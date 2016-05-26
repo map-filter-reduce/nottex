5c1249e7c693a599514a5ddc23332e38165ba49f
kasparpapli <kaspar.papli@gmail.com>
2016-05-26 15:35:09 +0300

    ToDoT

--- src/main/java/grammar/NottexXmlTranslator.java
+++ src/main/java/grammar/NottexXmlTranslator.java
@@ -7,12 +7,7 @@ public class NottexXmlTranslator {
 
     public XNode translate(NottexNode nottexNode) {
 
-
-
-
-
-
-
+        // TODO: Generate XML from reduced nottex tree
         throw new UnsupportedOperationException();
     }
 
