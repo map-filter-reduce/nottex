664a34dd66bf9cbd654062af5e8a7e9a3cdad6ef
kasparpapli <kaspar.papli@gmail.com>
2016-05-27 16:11:25 +0300

    Had lunch after reducing some functions, then continued to reduce some functions

--- src/test/java/NottexAstTextTest.java
+++ src/test/java/NottexAstTextTest.java
@@ -2,16 +2,26 @@ import nottex_ast.BlockNode;
 import nottex_ast.TextNode;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static grammar.AstParser.parse;
 
 public class NottexAstTextTest {
 
-
     @Test
-    public void testSimpleText() {
+    public void testSimpleText1() {
         String input = "text";
         BlockNode expected = new BlockNode(
                 new TextNode(input)
+        );
+        TestUtil.assertEquals(expected, parse(input));
+    }
+
+    @Test
+    public void testSimpleText2() {
+        String input = "text\n\nline";
+        BlockNode expected = new BlockNode(
+                new TextNode(Arrays.asList("text", "line"))
         );
         TestUtil.assertEquals(expected, parse(input));
     }
