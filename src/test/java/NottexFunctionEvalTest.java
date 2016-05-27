ef131ab1c245792e7afbbbe49f30340d6d45356a
PriitPaluoja <priitpaluoja@gmail.com>
2016-05-27 22:39:30 +0300

    GG WP

--- src/test/java/NottexFunctionEvalTest.java
+++ src/test/java/NottexFunctionEvalTest.java
@@ -1,8 +1,14 @@
 
 
+import grammar.FunctionReducer;
 import nottex_ast.BlockNode;
-import nottex_ast.FunctionCallNode;
+import nottex_ast.TagUseNode;
+import nottex_ast.TextNode;
+import nottex_ast.literals.IntNode;
+import nottex_ast.literals.StringNode;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static grammar.AstParser.parse;
 
@@ -11,102 +17,90 @@ public class NottexFunctionEvalTest {
 
     @Test
     public void testSimpleNoArgs() {
-        String input = "::func()";
+        String input = "::testEmpty()";
         BlockNode expected = new BlockNode(
-                new FunctionCallNode("func")
+                new StringNode("TEST")
         );
-        TestUtil.assertEquals(expected, parse(input));
+        TestUtil.assertEquals(expected, FunctionReducer.reduceFunctions(parse(input)));
     }
 
 
     @Test
     public void testSimpleArgs1() {
-        String input1 = "::func(\"\")";
+        String input1 = "::testUpperCase(\"\")";
         BlockNode expected1 = new BlockNode(
-                new FunctionCallNode("func", TestUtil.stringArg(""))
-        );
-        TestUtil.assertEquals(expected1, parse(input1));
+                new StringNode(""));
+        TestUtil.assertEquals(expected1, FunctionReducer.reduceFunctions(parse(input1)));
     }
 
 
     @Test
     public void testSimpleArgs2() {
-        String input2 = "::func(\"abc\")";
+        String input2 = "::testUpperCase(\"abc\")";
         BlockNode expected2 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.stringArg("abc"))
+                new StringNode("ABC")
         );
-        TestUtil.assertEquals(expected2, parse(input2));
+        TestUtil.assertEquals(expected2, FunctionReducer.reduceFunctions(parse(input2)));
     }
 
 
     @Test
     public void testSimpleArgs3() {
-        String input3 = "::func(\"mam\", \"\")";
+        String input3 = "::testRepeat(\"g\",5)";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.stringArg("mam"),
-                        TestUtil.stringArg(""))
+                new StringNode("ggggg")
         );
-        TestUtil.assertEquals(expected3, parse(input3));
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
 
     @Test
     public void testSimpleArgs4() {
-        String input3 = "::func(::fa())";
+        String input3 = "::testAdd(::testAdd(1,3),5)";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.funcArg(new FunctionCallNode("fa")))
+                new IntNode(9)
         );
-        TestUtil.assertEquals(expected3, parse(input3));
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
     @Test
     public void testSimpleArgs5() {
-        String input3 = "::func(::fa(), \"af\")";
+        String input3 = "::testRepeat(\"af\",::testAdd(1,4))";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.funcArg(new FunctionCallNode("fa")),
-                        TestUtil.stringArg("af"))
-        );
-        TestUtil.assertEquals(expected3, parse(input3));
+                new StringNode("afafafafaf"));
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
     @Test
-    public void testSimpleArgs6() {
-        String input3 = "::func(::fa(\"a\"), \"af\")";
+    public void testFunctionInTag6() {
+        String input3 = ",,tag{::testRepeat(\"af\",::testAdd(1,4))text}";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.funcArg(
-                                new FunctionCallNode("fa",
-                                        TestUtil.stringArg("a"))
-                        ),
-                        TestUtil.stringArg("af"))
+                new TagUseNode(
+                        Arrays.asList(
+                                new StringNode("afafafafaf"), new TextNode("text")), "tag")
         );
-        TestUtil.assertEquals(expected3, parse(input3));
+
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
 
     @Test
     public void testWhitespace1() {
-        String input3 = "::func  (  \"af\")";
+        String input3 = "::testUpperCase  (  \"af\")";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.stringArg("af"))
+                new StringNode("AF")
         );
-        TestUtil.assertEquals(expected3, parse(input3));
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
 
     @Test
     public void testWhitespace2() {
-        String input3 = "::func(  \"af \" )";
+        String input3 = "::testUpperCase  (  \"af\"  )";
         BlockNode expected3 = new BlockNode(
-                new FunctionCallNode("func",
-                        TestUtil.stringArg("af "))
+                new StringNode("AF")
         );
-        TestUtil.assertEquals(expected3, parse(input3));
+        TestUtil.assertEquals(expected3, FunctionReducer.reduceFunctions(parse(input3)));
     }
 
 }
