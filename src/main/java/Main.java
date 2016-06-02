b2f16f22afd0536e8e2ac207603062aa285dc781
kasparpapli <kaspar.papli@gmail.com>
2016-06-02 19:59:24 +0300

    Empty bottles around us

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,7 @@ import nottexast.NottexNode;
 import org.w3c.dom.Document;
 import pdfgen.PDFCreator;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -24,25 +21,45 @@ public class Main {
 
     public static void main(String[] args) throws IOException, DocumentException, TransformerException {
 
-        //TODO: line separators not working.
-        String input = getFileContent("src\\main\\resources\\input.ntex");
+        compile(args);
 
+//        String input = getFileContent("src\\main\\resources\\input.ntex");
+//        NottexNode astTree = AstParser.parse(input);
+//        NottexNode reducedTree = FunctionReducer.reduceFunctions(astTree);
+//        Document xmlDocument = createDocument(reducedTree);
+//        PDFCreator.convertDocumentToPDF(xmlDocument, "src\\main\\resources\\test.pdf");
+//
+//        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+//        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+//        //Create XML
+//        DOMSource source = new DOMSource(xmlDocument);
+//        StreamResult result = new StreamResult(new File("src\\main\\resources\\test.xhtml"));
+//        transformer.transform(source, result);
+    }
 
+    private static void compile(String[] args) throws IOException, DocumentException, TransformerException {
+        String input = getFileContent(args[0]);
         NottexNode astTree = AstParser.parse(input);
         NottexNode reducedTree = FunctionReducer.reduceFunctions(astTree);
         Document xmlDocument = createDocument(reducedTree);
-        PDFCreator.convertDocumentToPDF(xmlDocument, "src\\main\\resources\\test.pdf");
+        String inFilename = new File(args[0]).getAbsolutePath();
+        String outFilename = inFilename.substring(0, inFilename.lastIndexOf(".")) + ".pdf";
+        PDFCreator.convertDocumentToPDF(xmlDocument, outFilename);
 
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        //Create XML
-        DOMSource source = new DOMSource(xmlDocument);
-        StreamResult result = new StreamResult(new File("src\\main\\resources\\test.xhtml"));
-        transformer.transform(source, result);
+        if (false) {
+            // Create XML
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(xmlDocument);
+            StreamResult result = new StreamResult(new File("src\\main\\resources\\test.xhtml"));
+            transformer.transform(source, result);
+        }
+
     }
 
 
-    public static String getFileContent(String filePath) throws IOException {
+
+    private static String getFileContent(String filePath) throws IOException {
         return Files.readAllLines(Paths.get(filePath)).stream().collect(Collectors.joining("\n"));
     }
 }
