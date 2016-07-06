14978f9ce398aa163f65bcb3d1d4f2d6749f3ee3
kasparpapli <kaspar.papli@gmail.com>
2016-07-06 20:32:11 +0300

    sudo how many times has the has the times how many has

--- src/gen/java/grammar/nottexParser.java
+++ src/gen/java/grammar/nottexParser.java
@@ -18,8 +18,8 @@ public class NottexParser extends Parser {
 		new PredictionContextCache();
 	public static final int
 		TAG_MARKER=1, COMMA=2, FUNCTION_MARKER=3, COLON=4, LPAREN=5, RPAREN=6, 
-		LBRACE=7, RBRACE=8, QUOTE=9, PLUS=10, MINUS=11, MULTIP=12, DIVIS=13, WS=14, 
-		NUMBER=15, ALPHA=16;
+		LBRACE=7, RBRACE=8, ESCAPED_QUOTE=9, ESCAPED_BACKSLASH=10, BACKSLASH=11, 
+		QUOTE=12, PLUS=13, MINUS=14, MULTIP=15, DIVIS=16, WS=17, NUMBER=18, ALPHA=19;
 	public static final int
 		RULE_markupText = 0, RULE_functionCall = 1, RULE_tagUse = 2, RULE_tagIdens = 3, 
 		RULE_text = 4, RULE_funcArgs = 5, RULE_funcArg = 6, RULE_expr = 7, RULE_sub = 8, 
@@ -30,13 +30,13 @@ public class NottexParser extends Parser {
 	};
 
 	private static final String[] _LITERAL_NAMES = {
-		null, "',,'", "','", "'::'", "':'", "'('", "')'", "'{'", "'}'", "'\"'", 
-		"'+'", "'-'", "'*'", "'/'"
+		null, "',,'", "','", "'::'", "':'", "'('", "')'", "'{'", "'}'", "'\\\"'", 
+		"'\\\\'", "'\\'", "'\"'", "'+'", "'-'", "'*'", "'/'"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
 		null, "TAG_MARKER", "COMMA", "FUNCTION_MARKER", "COLON", "LPAREN", "RPAREN", 
-		"LBRACE", "RBRACE", "QUOTE", "PLUS", "MINUS", "MULTIP", "DIVIS", "WS", 
-		"NUMBER", "ALPHA"
+		"LBRACE", "RBRACE", "ESCAPED_QUOTE", "ESCAPED_BACKSLASH", "BACKSLASH", 
+		"QUOTE", "PLUS", "MINUS", "MULTIP", "DIVIS", "WS", "NUMBER", "ALPHA"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -159,6 +159,9 @@ public class NottexParser extends Parser {
 					case RPAREN:
 					case LBRACE:
 					case RBRACE:
+					case ESCAPED_QUOTE:
+					case ESCAPED_BACKSLASH:
+					case BACKSLASH:
 					case QUOTE:
 					case PLUS:
 					case MINUS:
@@ -516,6 +519,9 @@ public class NottexParser extends Parser {
 		public Token MINUS;
 		public Token MULTIP;
 		public Token DIVIS;
+		public Token ESCAPED_QUOTE;
+		public Token BACKSLASH;
+		public Token ESCAPED_BACKSLASH;
 		public Token WS;
 		public Token _tset121;
 		public List<TerminalNode> ALPHA() { return getTokens(NottexParser.ALPHA); }
@@ -570,6 +576,18 @@ public class NottexParser extends Parser {
 		public TerminalNode DIVIS(int i) {
 			return getToken(NottexParser.DIVIS, i);
 		}
+		public List<TerminalNode> ESCAPED_QUOTE() { return getTokens(NottexParser.ESCAPED_QUOTE); }
+		public TerminalNode ESCAPED_QUOTE(int i) {
+			return getToken(NottexParser.ESCAPED_QUOTE, i);
+		}
+		public List<TerminalNode> BACKSLASH() { return getTokens(NottexParser.BACKSLASH); }
+		public TerminalNode BACKSLASH(int i) {
+			return getToken(NottexParser.BACKSLASH, i);
+		}
+		public List<TerminalNode> ESCAPED_BACKSLASH() { return getTokens(NottexParser.ESCAPED_BACKSLASH); }
+		public TerminalNode ESCAPED_BACKSLASH(int i) {
+			return getToken(NottexParser.ESCAPED_BACKSLASH, i);
+		}
 		public List<TerminalNode> WS() { return getTokens(NottexParser.WS); }
 		public TerminalNode WS(int i) {
 			return getToken(NottexParser.WS, i);
@@ -612,7 +630,7 @@ public class NottexParser extends Parser {
 					setState(79);
 					((TextContext)_localctx)._tset121 = _input.LT(1);
 					_la = _input.LA(1);
-					if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << COMMA) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << QUOTE) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) ) {
+					if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << COMMA) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << ESCAPED_QUOTE) | (1L << ESCAPED_BACKSLASH) | (1L << BACKSLASH) | (1L << QUOTE) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) ) {
 						((TextContext)_localctx)._tset121 = (Token)_errHandler.recoverInline(this);
 					} else {
 						consume();
@@ -1332,8 +1350,11 @@ public class NottexParser extends Parser {
 		public Token MINUS;
 		public Token MULTIP;
 		public Token DIVIS;
+		public Token ESCAPED_QUOTE;
+		public Token BACKSLASH;
 		public Token WS;
-		public Token _tset344;
+		public Token ESCAPED_BACKSLASH;
+		public Token _tset348;
 		public List<TerminalNode> QUOTE() { return getTokens(NottexParser.QUOTE); }
 		public TerminalNode QUOTE(int i) {
 			return getToken(NottexParser.QUOTE, i);
@@ -1394,9 +1415,21 @@ public class NottexParser extends Parser {
 		public TerminalNode DIVIS(int i) {
 			return getToken(NottexParser.DIVIS, i);
 		}
+		public List<TerminalNode> ESCAPED_QUOTE() { return getTokens(NottexParser.ESCAPED_QUOTE); }
+		public TerminalNode ESCAPED_QUOTE(int i) {
+			return getToken(NottexParser.ESCAPED_QUOTE, i);
+		}
+		public List<TerminalNode> BACKSLASH() { return getTokens(NottexParser.BACKSLASH); }
+		public TerminalNode BACKSLASH(int i) {
+			return getToken(NottexParser.BACKSLASH, i);
+		}
 		public List<TerminalNode> WS() { return getTokens(NottexParser.WS); }
 		public TerminalNode WS(int i) {
 			return getToken(NottexParser.WS, i);
+		}
+		public List<TerminalNode> ESCAPED_BACKSLASH() { return getTokens(NottexParser.ESCAPED_BACKSLASH); }
+		public TerminalNode ESCAPED_BACKSLASH(int i) {
+			return getToken(NottexParser.ESCAPED_BACKSLASH, i);
 		}
 		public StringContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
@@ -1429,18 +1462,18 @@ public class NottexParser extends Parser {
 			setState(146);
 			_errHandler.sync(this);
 			_la = _input.LA(1);
-			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << TAG_MARKER) | (1L << COMMA) | (1L << FUNCTION_MARKER) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) {
+			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << TAG_MARKER) | (1L << COMMA) | (1L << FUNCTION_MARKER) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << ESCAPED_QUOTE) | (1L << ESCAPED_BACKSLASH) | (1L << BACKSLASH) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) {
 				{
 				{
 				setState(143);
-				((StringContext)_localctx)._tset344 = _input.LT(1);
+				((StringContext)_localctx)._tset348 = _input.LT(1);
 				_la = _input.LA(1);
-				if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << TAG_MARKER) | (1L << COMMA) | (1L << FUNCTION_MARKER) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) ) {
-					((StringContext)_localctx)._tset344 = (Token)_errHandler.recoverInline(this);
+				if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << TAG_MARKER) | (1L << COMMA) | (1L << FUNCTION_MARKER) | (1L << COLON) | (1L << LPAREN) | (1L << RPAREN) | (1L << LBRACE) | (1L << RBRACE) | (1L << ESCAPED_QUOTE) | (1L << ESCAPED_BACKSLASH) | (1L << BACKSLASH) | (1L << PLUS) | (1L << MINUS) | (1L << MULTIP) | (1L << DIVIS) | (1L << WS) | (1L << NUMBER) | (1L << ALPHA))) != 0)) ) {
+					((StringContext)_localctx)._tset348 = (Token)_errHandler.recoverInline(this);
 				} else {
 					consume();
 				}
-				((StringContext)_localctx).content.add(((StringContext)_localctx)._tset344);
+				((StringContext)_localctx).content.add(((StringContext)_localctx)._tset348);
 				}
 				}
 				setState(148);
@@ -1551,7 +1584,7 @@ public class NottexParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\22\u00a0\4\2\t\2"+
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\25\u00a0\4\2\t\2"+
 		"\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13"+
 		"\t\13\4\f\t\f\4\r\t\r\3\2\3\2\3\2\6\2\36\n\2\r\2\16\2\37\3\3\3\3\3\3\7"+
 		"\3%\n\3\f\3\16\3(\13\3\3\3\3\3\3\3\3\3\5\3.\n\3\3\3\3\3\3\3\3\4\3\4\3"+
@@ -1563,37 +1596,37 @@ public class NottexParser extends Parser {
 		"\7\n\u0085\n\n\f\n\16\n\u0088\13\n\3\13\3\13\3\13\3\13\3\13\5\13\u008f"+
 		"\n\13\3\f\3\f\7\f\u0093\n\f\f\f\16\f\u0096\13\f\3\f\3\f\3\r\7\r\u009b"+
 		"\n\r\f\r\16\r\u009e\13\r\3\r\2\4\20\22\16\2\4\6\b\n\f\16\20\22\24\26\30"+
-		"\2\5\3\2\21\22\4\2\4\4\6\22\4\2\3\n\f\22\u00a9\2\35\3\2\2\2\4!\3\2\2\2"+
-		"\6\62\3\2\2\2\b;\3\2\2\2\nR\3\2\2\2\fV\3\2\2\2\16d\3\2\2\2\20f\3\2\2\2"+
-		"\22|\3\2\2\2\24\u008e\3\2\2\2\26\u0090\3\2\2\2\30\u009c\3\2\2\2\32\36"+
+		"\2\5\3\2\24\25\4\2\4\4\6\25\4\2\3\r\17\25\u00a9\2\35\3\2\2\2\4!\3\2\2"+
+		"\2\6\62\3\2\2\2\b;\3\2\2\2\nR\3\2\2\2\fV\3\2\2\2\16d\3\2\2\2\20f\3\2\2"+
+		"\2\22|\3\2\2\2\24\u008e\3\2\2\2\26\u0090\3\2\2\2\30\u009c\3\2\2\2\32\36"+
 		"\5\4\3\2\33\36\5\6\4\2\34\36\5\n\6\2\35\32\3\2\2\2\35\33\3\2\2\2\35\34"+
 		"\3\2\2\2\36\37\3\2\2\2\37\35\3\2\2\2\37 \3\2\2\2 \3\3\2\2\2!\"\7\5\2\2"+
-		"\"&\7\22\2\2#%\t\2\2\2$#\3\2\2\2%(\3\2\2\2&$\3\2\2\2&\'\3\2\2\2\')\3\2"+
+		"\"&\7\25\2\2#%\t\2\2\2$#\3\2\2\2%(\3\2\2\2&$\3\2\2\2&\'\3\2\2\2\')\3\2"+
 		"\2\2(&\3\2\2\2)*\5\30\r\2*+\7\7\2\2+-\5\30\r\2,.\5\f\7\2-,\3\2\2\2-.\3"+
 		"\2\2\2./\3\2\2\2/\60\5\30\r\2\60\61\7\b\2\2\61\5\3\2\2\2\62\63\7\3\2\2"+
 		"\63\64\5\b\5\2\64\65\5\30\r\2\65\67\7\t\2\2\668\5\2\2\2\67\66\3\2\2\2"+
-		"\678\3\2\2\289\3\2\2\29:\7\n\2\2:\7\3\2\2\2;?\7\22\2\2<>\t\2\2\2=<\3\2"+
+		"\678\3\2\2\289\3\2\2\29:\7\n\2\2:\7\3\2\2\2;?\7\25\2\2<>\t\2\2\2=<\3\2"+
 		"\2\2>A\3\2\2\2?=\3\2\2\2?@\3\2\2\2@N\3\2\2\2A?\3\2\2\2BC\5\30\r\2CD\7"+
-		"\4\2\2DE\5\30\r\2EI\7\22\2\2FH\t\2\2\2GF\3\2\2\2HK\3\2\2\2IG\3\2\2\2I"+
+		"\4\2\2DE\5\30\r\2EI\7\25\2\2FH\t\2\2\2GF\3\2\2\2HK\3\2\2\2IG\3\2\2\2I"+
 		"J\3\2\2\2JM\3\2\2\2KI\3\2\2\2LB\3\2\2\2MP\3\2\2\2NL\3\2\2\2NO\3\2\2\2"+
 		"O\t\3\2\2\2PN\3\2\2\2QS\t\3\2\2RQ\3\2\2\2ST\3\2\2\2TR\3\2\2\2TU\3\2\2"+
 		"\2U\13\3\2\2\2V^\5\16\b\2WX\5\30\r\2XY\7\4\2\2YZ\5\30\r\2Z[\5\16\b\2["+
 		"]\3\2\2\2\\W\3\2\2\2]`\3\2\2\2^\\\3\2\2\2^_\3\2\2\2_\r\3\2\2\2`^\3\2\2"+
 		"\2ae\5\26\f\2be\5\20\t\2ce\5\4\3\2da\3\2\2\2db\3\2\2\2dc\3\2\2\2e\17\3"+
-		"\2\2\2fg\b\t\1\2gh\5\22\n\2hq\3\2\2\2ij\f\5\2\2jk\7\f\2\2kp\5\22\n\2l"+
-		"m\f\4\2\2mn\7\r\2\2np\5\22\n\2oi\3\2\2\2ol\3\2\2\2ps\3\2\2\2qo\3\2\2\2"+
-		"qr\3\2\2\2r\21\3\2\2\2sq\3\2\2\2tv\b\n\1\2uw\7\r\2\2vu\3\2\2\2wx\3\2\2"+
-		"\2xv\3\2\2\2xy\3\2\2\2yz\3\2\2\2z}\5\24\13\2{}\5\24\13\2|t\3\2\2\2|{\3"+
-		"\2\2\2}\u0086\3\2\2\2~\177\f\6\2\2\177\u0080\7\16\2\2\u0080\u0085\5\24"+
-		"\13\2\u0081\u0082\f\5\2\2\u0082\u0083\7\17\2\2\u0083\u0085\5\24\13\2\u0084"+
-		"~\3\2\2\2\u0084\u0081\3\2\2\2\u0085\u0088\3\2\2\2\u0086\u0084\3\2\2\2"+
-		"\u0086\u0087\3\2\2\2\u0087\23\3\2\2\2\u0088\u0086\3\2\2\2\u0089\u008a"+
-		"\7\7\2\2\u008a\u008b\5\20\t\2\u008b\u008c\7\b\2\2\u008c\u008f\3\2\2\2"+
-		"\u008d\u008f\7\21\2\2\u008e\u0089\3\2\2\2\u008e\u008d\3\2\2\2\u008f\25"+
-		"\3\2\2\2\u0090\u0094\7\13\2\2\u0091\u0093\t\4\2\2\u0092\u0091\3\2\2\2"+
-		"\u0093\u0096\3\2\2\2\u0094\u0092\3\2\2\2\u0094\u0095\3\2\2\2\u0095\u0097"+
-		"\3\2\2\2\u0096\u0094\3\2\2\2\u0097\u0098\7\13\2\2\u0098\27\3\2\2\2\u0099"+
-		"\u009b\7\20\2\2\u009a\u0099\3\2\2\2\u009b\u009e\3\2\2\2\u009c\u009a\3"+
+		"\2\2\2fg\b\t\1\2gh\5\22\n\2hq\3\2\2\2ij\f\5\2\2jk\7\17\2\2kp\5\22\n\2"+
+		"lm\f\4\2\2mn\7\20\2\2np\5\22\n\2oi\3\2\2\2ol\3\2\2\2ps\3\2\2\2qo\3\2\2"+
+		"\2qr\3\2\2\2r\21\3\2\2\2sq\3\2\2\2tv\b\n\1\2uw\7\20\2\2vu\3\2\2\2wx\3"+
+		"\2\2\2xv\3\2\2\2xy\3\2\2\2yz\3\2\2\2z}\5\24\13\2{}\5\24\13\2|t\3\2\2\2"+
+		"|{\3\2\2\2}\u0086\3\2\2\2~\177\f\6\2\2\177\u0080\7\21\2\2\u0080\u0085"+
+		"\5\24\13\2\u0081\u0082\f\5\2\2\u0082\u0083\7\22\2\2\u0083\u0085\5\24\13"+
+		"\2\u0084~\3\2\2\2\u0084\u0081\3\2\2\2\u0085\u0088\3\2\2\2\u0086\u0084"+
+		"\3\2\2\2\u0086\u0087\3\2\2\2\u0087\23\3\2\2\2\u0088\u0086\3\2\2\2\u0089"+
+		"\u008a\7\7\2\2\u008a\u008b\5\20\t\2\u008b\u008c\7\b\2\2\u008c\u008f\3"+
+		"\2\2\2\u008d\u008f\7\24\2\2\u008e\u0089\3\2\2\2\u008e\u008d\3\2\2\2\u008f"+
+		"\25\3\2\2\2\u0090\u0094\7\16\2\2\u0091\u0093\t\4\2\2\u0092\u0091\3\2\2"+
+		"\2\u0093\u0096\3\2\2\2\u0094\u0092\3\2\2\2\u0094\u0095\3\2\2\2\u0095\u0097"+
+		"\3\2\2\2\u0096\u0094\3\2\2\2\u0097\u0098\7\16\2\2\u0098\27\3\2\2\2\u0099"+
+		"\u009b\7\23\2\2\u009a\u0099\3\2\2\2\u009b\u009e\3\2\2\2\u009c\u009a\3"+
 		"\2\2\2\u009c\u009d\3\2\2\2\u009d\31\3\2\2\2\u009e\u009c\3\2\2\2\26\35"+
 		"\37&-\67?INT^doqx|\u0084\u0086\u008e\u0094\u009c";
 	public static final ATN _ATN =
